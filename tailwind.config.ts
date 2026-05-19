e9eb0b3c3b3470aee2fff716be5a2dfb65fef670
Ramazan Arıduru <ramazanariduru@Ramazan-Air.station>
2026-05-20 00:02:27 +0300

    fix: tailwind syntax + standalone start

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -43,16 +43,16 @@ const config: Config = {
           500: "#f59e0b",
         },
       },
-      },
+      
       keyframes: {
         'fade-in-up': {
           '0%': { opacity: '0', transform: 'translateY(10px)' },
           '100%': { opacity: '1', transform: 'translateY(0)' },
-        }
+        },
       },
       animation: {
         'fade-in-up': 'fade-in-up 0.5s ease-out forwards',
-      }
+      },
     },
   },
   plugins: [],
